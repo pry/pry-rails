8035591863ed0eed5488a8ee123f7784e30bb6f5
Han <hkfortytwo@gmail.com>
2011-12-27 14:07:36 -0800

    make pry-rails friendly to rails 3.2 by removing uninitialized constant error

--- lib/pry-rails.rb
+++ lib/pry-rails.rb
@@ -1,16 +1,17 @@
+require 'rails'
 require "pry-rails/version"
 
 module PryRails
-  if defined? Rails && Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR >= 1
+  if defined? Rails && Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR >= 0
     class Railtie < ::Rails::Railtie
       silence_warnings do
         begin
           require 'pry'
           ::IRB = Pry
-        rescue LoadError
+          IRB::ExtendCommandBundle = Pry
+          rescue LoadError 
         end
       end
     end
   end
 end
-
