64aaf0bec385ae589752ade0fd2a3a9ea6814691
Robin Wenglewski <robin@wenglewski.de>
2012-01-11 20:25:46 +0100

    changes of 1.0.0 gem

--- lib/pry-rails.rb
+++ lib/pry-rails.rb
@@ -1,12 +1,13 @@
 require "pry-rails/version"
 
 module PryRails
-  if(defined?(::Rails) and ::Rails.version >= "3")
+  if(defined?(::Rails) and ::Rails::VERSION::MAJOR >= 3)
     class Railtie < ::Rails::Railtie
       silence_warnings do
         begin
           require 'pry'
           ::IRB = Pry
+          IRB::ExtendCommandBundle = Pry
         rescue LoadError
         end
       end
--- lib/pry-rails/version.rb
+++ lib/pry-rails/version.rb
@@ -1,3 +1,3 @@
 module PryRails
-  VERSION = "0.0.5"
+  VERSION = "0.1.1"
 end
