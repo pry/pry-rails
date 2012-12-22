5f31f61da5c970294a3a680fd9ed349a1a7ae699
Takatoshi Matsumoto <toqoz403@gmail.com>
2012-12-22 11:43:09 +0900

    Disable when DISABLE_PRY_RAILS is given as env variable
    I love pry-rails.
    But sometimes I would like to or need to disable pry-rails.

--- lib/pry-rails/railtie.rb
+++ lib/pry-rails/railtie.rb
@@ -3,26 +3,28 @@
 module PryRails
   class Railtie < Rails::Railtie
     console do
-      require 'pry'
-      require 'pry-rails/commands'
+      if !ENV["DISABLE_PRY_RAILS"]
+        require 'pry'
+        require 'pry-rails/commands'
 
-      if Rails::VERSION::MAJOR == 3
-        Rails::Console::IRB = Pry
+        if Rails::VERSION::MAJOR == 3
+          Rails::Console::IRB = Pry
 
-        unless defined? Pry::ExtendCommandBundle
-          Pry::ExtendCommandBundle = Module.new
+          unless defined? Pry::ExtendCommandBundle
+            Pry::ExtendCommandBundle = Module.new
+          end
         end
-      end
 
-      if Rails::VERSION::MAJOR == 4
-        Rails.application.config.console = Pry
-      end
+        if Rails::VERSION::MAJOR == 4
+          Rails.application.config.console = Pry
+        end
 
-      if (Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR >= 2) ||
-          Rails::VERSION::MAJOR == 4
-        require "rails/console/app"
-        require "rails/console/helpers"
-        TOPLEVEL_BINDING.eval('self').extend ::Rails::ConsoleMethods
+        if (Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR >= 2) ||
+            Rails::VERSION::MAJOR == 4
+          require "rails/console/app"
+          require "rails/console/helpers"
+          TOPLEVEL_BINDING.eval('self').extend ::Rails::ConsoleMethods
+        end
       end
     end
   end
