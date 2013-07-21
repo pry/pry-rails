1860f507aeede8f58ae1d2838c2a4fec99419446
Ryan Fitzgerald <rwfitzge@gmail.com>
2013-07-20 21:45:55 -0700

    Extract model formatting from show-models into ModelFormatter

--- lib/pry-rails.rb
+++ lib/pry-rails.rb
@@ -6,4 +6,5 @@ require 'pry-rails/version'
 if defined?(Rails) && !ENV['DISABLE_PRY_RAILS']
   require 'pry-rails/railtie'
   require 'pry-rails/commands'
+  require 'pry-rails/model_formatter'
 end
--- lib/pry-rails/commands/show_models.rb
+++ lib/pry-rails/commands/show_models.rb
@@ -18,6 +18,8 @@ class PryRails::ShowModels < Pry::ClassCommand
   def process
     Rails.application.eager_load!
 
+    @formatter = PryRails::ModelFormatter.new
+
     display_activerecord_models
     display_mongoid_models
   end
@@ -28,32 +30,7 @@ class PryRails::ShowModels < Pry::ClassCommand
     models = ActiveRecord::Base.descendants
 
     models.sort_by(&:to_s).each do |model|
-      out = []
-      out.push format_model_name model
-
-      if model.table_exists?
-        model.columns.each do |column|
-          out.push format_column column.name, column.type
-        end
-      else
-        out.push format_error "Table doesn't exist"
-      end
-
-      reflections = model.reflections.sort_by do |other_model, reflection|
-        [reflection.macro.to_s, other_model.to_s]
-      end
-
-      reflections.each do |other_model, reflection|
-        options = []
-
-        if reflection.options[:through].present?
-          options << "through #{text.blue ":#{reflection.options[:through]}"}"
-        end
-
-        out.push format_association reflection.macro, other_model, options
-      end
-
-      print_unless_filtered out
+      print_unless_filtered @formatter.format_active_record(model)
     end
   end
 
@@ -75,55 +52,16 @@ class PryRails::ShowModels < Pry::ClassCommand
     end
 
     models.sort_by(&:to_s).each do |model|
-      out = []
-      out.push format_model_name model
-
-      model.fields.values.sort_by(&:name).each do |column|
-        out.push format_column column.name, column.options[:type]
-      end
-
-      model.relations.each do |other_model, ref|
-        options = []
-        options << 'autosave'  if ref.options[:autosave]
-        options << 'autobuild' if ref.options[:autobuild]
-        options << 'validate'  if ref.options[:validate]
-
-        if ref.options[:dependent]
-          options << "dependent-#{ref.options[:dependent]}"
-        end
-
-        out.push format_association \
-          kind_of_relation(ref.relation), other_model, options
-      end
-
-      print_unless_filtered out
+      print_unless_filtered @formatter.format_mongoid(model)
     end
   end
 
-  def format_model_name(model)
-    text.bright_blue model
-  end
-
-  def format_column(name, type)
-    "  #{name}: #{text.green type}"
-  end
-
-  def format_association(type, other, options = [])
-    options_string = (options.any?) ? " (#{options.join(', ')})" : ''
-    "  #{type} #{text.blue ":#{other}"}#{options_string}"
-  end
-
-  def format_error(message)
-    "  #{text.red message}"
-  end
-
-  def print_unless_filtered array_of_strings
-    result = array_of_strings.join("\n")
+  def print_unless_filtered(str)
     if opts.present?(:G)
-      return unless result =~ grep_regex
-      result = colorize_matches(result) # :(
+      return unless str =~ grep_regex
+      str = colorize_matches(str) # :(
     end
-    output.puts result
+    output.puts str
   end
 
   def colorize_matches(string)
@@ -136,17 +74,6 @@ class PryRails::ShowModels < Pry::ClassCommand
 
   def grep_regex
     @grep_regex ||= Regexp.new(opts[:G], Regexp::IGNORECASE)
-  end
-
-  def kind_of_relation(relation)
-    case relation.to_s.sub(/^Mongoid::Relations::/, '')
-      when 'Referenced::Many' then 'has_many'
-      when 'Referenced::One'  then 'has_one'
-      when 'Referenced::In'   then 'belongs_to'
-      when 'Embedded::Many'   then 'embeds_many'
-      when 'Embedded::One'    then 'embeds_one'
-      when 'Embedded::In'     then 'embedded_in'
-    end
   end
 end
 
